18d5184c683bc8be1f77e243b2acd6b41c73a056
Mason Malone <651224+MasonM@users.noreply.github.com>
2026-08-09 20:59:53 -0700

    Tweak generate.sh
    Signed-off-by: Mason Malone <651224+MasonM@users.noreply.github.com>

--- generate.sh
+++ generate.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+set -euxo pipefail
 PARAMS='-D inner_length=322 -D wall_th=3.1 -D hitchpin_block_th=6'
-./lasercut/convert-2d.py $PARAMS -D generate_only=3.2 monochord_lasercut.scad render/monochord_lasercut_3mm.svg
+./lasercut/convert-2d.py $PARAMS -D generate_only=3.1 monochord_lasercut.scad render/monochord_lasercut_3mm.svg
 ./lasercut/convert-2d.py $PARAMS -D generate_only=6 monochord_lasercut.scad render/monochord_lasercut_6mm.svg
